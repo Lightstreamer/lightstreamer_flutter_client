c9c8539af0061c324891d54126fbe9995b3f9674
acarioni <alessandro.carioni@lightstreamer.com>
2024-11-08 10:03:44 +0100

    add macos platform

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import lightstreamer_flutter_client
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LightstreamerFlutterPlugin.register(with: registry.registrar(forPlugin: "LightstreamerFlutterPlugin"))
+}
